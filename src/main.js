d525d5e021529e5434a31e67d5e9a4a48a09ffd2
Joseph Isbell <shadowwolf347@comcast.net>
2018-11-02 10:01:58 -0600

    Added a modifier and used the setTimeout function to create a 3s delay.

--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ import App from './App.vue'
 Vue.directive('highlight', {
   bind(el, binding, vnode) {
     // el.style.backgroundColor = binding.value;
-    if(binding.arg == 'background'){
-      el.style.backgroundColor = binding.value;
-    } else {
-      el.style.color = binding.value;
+    var delay = 0;
+    if(binding.modifiers['delayed']){
+      delay = 3000;
     }
+    setTimeout(() => {
+      if(binding.arg == 'background'){
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
   }
 });
 
